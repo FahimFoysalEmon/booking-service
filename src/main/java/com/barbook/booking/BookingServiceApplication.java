2c511f430d8ab2c1b17e1cc89262937a6f7dc021
Fahim Foysal Emon <fahimfoysalemon@Fahims-MacBook-Pro.local>
2026-08-06 16:02:26 +0600

    Initial commit: slot booking service

---
+++ src/main/java/com/barbook/booking/BookingServiceApplication.java
@@ -0,0 +1,18 @@
+package com.barbook.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World, Finally Running the project!");
+
+		SpringApplication.run(BookingServiceApplication.class, args);
+
+
+	}
+
+}
