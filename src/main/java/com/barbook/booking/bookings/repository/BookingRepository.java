94a1f27d5be40dd87720b3c3bed34e2ab0c4401e
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-08-17 10:46:16 +0600

    repository annotation added for booking

--- src/main/java/com/barbook/booking/bookings/repository/BookingRepository.java
+++ src/main/java/com/barbook/booking/bookings/repository/BookingRepository.java
@@ -7,9 +7,12 @@ import com.barbook.booking.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Bookings, Long> {
 
     List<Bookings> findByCustomer(Users customer);
