e4aa5c474098d52044df366e60b5fec4df0dfd0d
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-08-20 13:08:16 +0600

    merging for login page react func learning

--- frontend/slot-booking-frontend/src/pages/LoginPage.jsx
+++ frontend/slot-booking-frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,27 @@
-
+import { useState } from "react"; //to remember the state of the form
+import { useNavigate } from "react-router-dom"; //to navigate to the home page
+import api from "../lib/api";
+import saveToken from "../lib/token"; //to save the token to the local storage
 
 export default function LoginPage() {
-  return <h1>Login Page</h1>;
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await api.post("api/v1/auth/login", {
+        email,
+        password,
+       });
+      saveToken(response.data.token);
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  }
 }
