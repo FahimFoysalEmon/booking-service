a289664b942cf6b6e79b7053be23a663bf6cdfea
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-09-15 15:51:58 +0600

    shop service page organizing

--- frontend/slot-booking-frontend/src/pages/ShopServicesPage.jsx
+++ frontend/slot-booking-frontend/src/pages/ShopServicesPage.jsx
@@ -1,9 +1,14 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../lib/api";
 
 
 export default function ShopServicesPage() {
 
     const { shopId } = useParams();
+    const [ services , setServices ] = useState([]);
+    const [ error , setError ] = useState("");
+    const [ loading, setLoading ] = useState(true);
 
     return (
 
