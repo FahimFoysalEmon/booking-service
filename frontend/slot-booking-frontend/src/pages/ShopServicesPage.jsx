653998e9b48e4a4e81448ecd3310b1cb933c9cf3
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-09-15 14:46:06 +0600

    shop service page data loding

--- frontend/slot-booking-frontend/src/pages/ShopServicesPage.jsx
+++ frontend/slot-booking-frontend/src/pages/ShopServicesPage.jsx
@@ -1,6 +1,16 @@
-
+import { useParams } from "react-router-dom"
 
 
 export default function ShopServicesPage() {
-    return <h1>Services</h1>
+
+    const { shopId } = useParams();
+
+    return (
+
+        <div>
+            <h1>Services</h1>
+            <p>Shop ID: {shopId}</p>
+        </div>
+
+    )
 }
