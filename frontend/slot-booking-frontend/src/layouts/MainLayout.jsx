0141bfbf6610448841094caeb917b3b5f1e53f61
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2026-09-23 17:48:01 +0600

    navbar added properly into the app

--- frontend/slot-booking-frontend/src/layouts/MainLayout.jsx
+++ frontend/slot-booking-frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { getToken, removeToken } from "../lib/token";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function MainLayout() {
 
@@ -13,26 +14,28 @@ export default function MainLayout() {
 
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        {!loggedIn && (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
-
-        <Link to="/shops">Shops</Link>
-
-        {loggedIn && (
-          <>
-            <Link to="/my-bookings">My Bookings</Link>
-            <button type="button" onClick={handleLogout}>
-              Logout
-            </button>
-          </>
-        )}
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Nav>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            {!loggedIn && (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/register">Register</Nav.Link>
+              </>
+            )}
+            <Nav.Link as={Link} to="/shops">Shops</Nav.Link>
+            {loggedIn && (
+              <>
+                <Nav.Link as={Link} to="/my-bookings">My Bookings</Nav.Link>
+                <Nav.Link as="button" onClick={handleLogout}>
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
       <Outlet />
     </div>
   );
